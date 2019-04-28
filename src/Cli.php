61d6ab99fdf2d1dea5c0e92c3ed07749ba40710f
harumimax <harumimax@gmail.com>
2019-04-28 19:18:15 +0300

    step 2

---
+++ src/Cli.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BrainGames\Cli;
+
+function run() {
+	\cli\line('Welcome to the Brain Game!');
+    $name = \cli\prompt('May I have your name?');
+    \cli\line("Hello, %s!", $name);
+}

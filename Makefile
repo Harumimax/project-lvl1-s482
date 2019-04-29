1ae74e7ae40b69f9026bddcaf20bc365c1aaa7d1
harumimax <harumimax@gmail.com>
2019-04-29 23:40:07 +0300

    step 4

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 install:
 	composer install
 
+update:
+	composer dump-autoload
+
 lint:
 	composer run-script phpcs -- --standard=PSR12 src bin
--- src/Cli.php
+++ src/Cli.php
@@ -2,8 +2,9 @@
 
 namespace BrainGames\Cli;
 
-function run() {
-	\cli\line('Welcome to the Brain Game!');
+function run()
+{
+    \cli\line('Welcome to the Brain Game!');
     $name = \cli\prompt('May I have your name?');
     \cli\line("Hello, %s!", $name);
 }
